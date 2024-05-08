86e5f9da8bbd9388f7f72641ed9ce4867d40b026
eduardo-novatek <eolivera@novateksoluciones.com>
2024-05-08 20:16:44 -0300

    trabajando en pantalla de usuarios y roles

--- lib/view/screens/user_and_role_screen.dart
+++ lib/view/screens/user_and_role_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class UserAndRoleScreen extends StatefulWidget {
   const UserAndRoleScreen({super.key});
@@ -10,11 +10,70 @@ class UserAndRoleScreen extends StatefulWidget {
 class _UserAndRoleScreenState extends State<UserAndRoleScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Contenido de Usuarios y Roles',
-        style: TextStyle(fontSize: 24),
-      ),
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2.0, color: Colors.black54),
+              borderRadius: BorderRadius.circular(18.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0),
+                  ),
+                  child: Container(
+                    color: Colors.blue, // Color de fondo del título
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Container 1', // Título del contenedor 1
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10), // Espacio entre el título y la lista de elementos
+                // Aquí puedes agregar la lista de elementos para el contenedor 1
+              ],
+            ),
+          ),
+        ),
+
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              border: Border.all(width: 2.0, color: Colors.black54),
+              borderRadius: BorderRadius.circular(18.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0),
+                  ),
+                  child: Container(
+                    color: Colors.green, // Color de fondo del título
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Container 2', // Título del contenedor 2
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10), // Espacio entre el título y la lista de elementos
+                // Aquí puedes agregar la lista de elementos para el contenedor 2
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
