bf294619b47c589a72d0c538ea3474420a3ef695
eduardo_UTEC <eduardo.olivera@estudiantes.utec.edu.uy>
2024-06-24 13:25:48 -0300

    voucher: medicamento controlado

--- lib/view/dialogs/controlled_medication_dialog.dart
+++ lib/view/dialogs/controlled_medication_dialog.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:novafarma_front/model/DTOs/controlled_medication_dto.dart';
 
+import '../../model/DTOs/customer_dto.dart';
 import '../../model/enums/data_type_enum.dart';
 import '../../model/globals/tools/create_text_form_field.dart';
 
 class ControlledMedicationDialog extends StatefulWidget {
   final ControlledMedicationDTO controlledMedication;  //Se actualiza con los datos seleccionados
+  //final CustomerDTO customer;
 
   const ControlledMedicationDialog({
     super.key,
     required this.controlledMedication,
+    //required this.customer,
   });
 
   @override
@@ -34,7 +37,19 @@ class _ControlledMedicationDialogState extends State<ControlledMedicationDialog>
   void initState() {
     super.initState();
     isAdd = (widget.controlledMedication.lastSaleDate == null);
-    print(isAdd);
+
+    if (isAdd) {
+      _frequencyDaysController.value = TextEditingValue(text: '0');
+      _toleranceDaysController.value = TextEditingValue(text: '0');
+      // Es modificacion
+    } else {
+      _frequencyDaysController.value = TextEditingValue(
+        text: widget.controlledMedication.frequencyDays.toString()
+      );
+      _toleranceDaysController.value = TextEditingValue(
+          text: widget.controlledMedication.toleranceDays.toString()
+      );
+    }
   }
 
   @override
@@ -66,10 +81,22 @@ class _ControlledMedicationDialogState extends State<ControlledMedicationDialog>
               children: [
                 Text(isAdd
                     ? 'Agregar medicamento controlado'
-                    : 'Modificar medicamento controlado',
-                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    : 'Actualizar medicamento controlado',
+                  style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 20),
+                widget.controlledMedication.lastSaleDate == null
+                  ? const Text(
+                      'Primera venta',
+                      style: TextStyle(fontStyle: FontStyle.italic, fontSize: 14,),
+                    )
+                  : const SizedBox.shrink(),
+                //Text('Paciente: ${widget.customer.name} ${widget.customer.lastname}'),
+                Text('Paciente: ${widget.controlledMedication.customerName}'),
+                Text('Medicamento: ${widget.controlledMedication.medicineName}'),
+                widget.controlledMedication.lastSaleDate != null
+                  ? Text('Ultima venta ${widget.controlledMedication.lastSaleDate}')
+                  : const SizedBox.shrink(),
                 Flexible(
                   child: SingleChildScrollView(
                     child: Form(
@@ -77,24 +104,17 @@ class _ControlledMedicationDialogState extends State<ControlledMedicationDialog>
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                         isAdd
-                              ? CreateTextFormField(
-                                  controller: _frequencyDaysController,
-                                  focusNode: _frequencyDaysFocusNode,
-                                  label: 'Frecuencia de compra',
-                                  dataType: DataTypeEnum.number,
-                                  minValueForValidation: 0,
-                                  maxValueForValidation: 365,
-                                  textForValidation: 'Ingrese un valor entre 0 y 365',
-                                  initialFocus: true,
-                                )
-                              : Text('Frecuencia de compra: '
-                                  '${widget.controlledMedication.frequencyDays}'),
-
+                          CreateTextFormField(
+                            controller: _frequencyDaysController,
+                            focusNode: _frequencyDaysFocusNode,
+                            label: 'Frecuencia de compra',
+                            dataType: DataTypeEnum.number,
+                            minValueForValidation: 0,
+                            maxValueForValidation: 365,
+                            textForValidation: 'Ingrese un valor entre 0 y 365',
+                            initialFocus: true,
+                          ),
                           const SizedBox(height: 20),
-                          //_buildTable(),
-                          const SizedBox(height: 5),
-
                           CreateTextFormField(
                             controller: _toleranceDaysController,
                             focusNode: _toleranceDaysFocusNode,
