18cc536ea479760147c6fb31111421d17f34d250
eduardo-novatek <eolivera@novateksoluciones.com>
2024-07-17 10:16:06 -0300

    clientes: agregar. Trabajando en layout

--- lib/view/screens/add_customer_screen.dart
+++ lib/view/screens/add_customer_screen.dart
@@ -15,11 +15,156 @@ class _AddCustomerState extends State<AddCustomerScreen> {
   final _nameController = TextEditingController();
   final _notesController = TextEditingController();
   final _telephoneController = TextEditingController();
-  final _paymentNumberController = TextEditingController();
-  final _userIdController = TextEditingController();
-  final _dependentIdController = TextEditingController();
-  final _partnerIdController = TextEditingController();
-  final _deletedController = TextEditingController();
+  //final _paymentNumberController = TextEditingController();
+
+  int _paymentNumbe = 0, _dependentId = 0, _partnerId = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.4,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+              width: 1.0,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // Ajusta el tamaño vertical al contenido
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildTitleBar(),
+              _buildBody(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildTitleBar() {
+    return Container(
+      color: Colors.blue,
+      padding: const EdgeInsets.all(8.0),
+      child: const Text(
+        'Agregar clientes',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 19.0,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min, // Ajusta el tamaño vertical al contenido
+          children: [
+            TextFormField(
+              controller: _documentController,
+              decoration: InputDecoration(labelText: 'Documento'),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Por favor, ingresa el documento';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _dateController,
+              decoration: InputDecoration(labelText: 'Fecha de ingreso'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Por favor, ingresa la fecha de ingreso';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _lastnameController,
+              decoration: InputDecoration(labelText: 'Apellido'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Por favor, ingresa el apellido';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: 'Nombre'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Por favor, ingresa el nombre';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _telephoneController,
+              decoration: InputDecoration(labelText: 'Teléfono'),
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Por favor, ingresa el teléfono';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _notesController,
+              decoration: InputDecoration(labelText: 'Notas'),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Por favor, ingresa las notas';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20.0),
+            _footerBody(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _footerBody() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              if (await _confirm() == 1) {
+                _submitForm();
+              }
+            },
+            child: const Text('Aceptar'),
+          ),
+          const SizedBox(width: 16.0,),
+          ElevatedButton(
+            onPressed: () async {
+              if (await _cancel() == 1) {
+                _cancelForm();
+              }
+            },
+            child: const Text('Cancelar'),
+          ),
+        ],
+      ),
+    );
+  }
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
@@ -35,11 +180,11 @@ class _AddCustomerState extends State<AddCustomerScreen> {
 
   Future<int> _confirm() async {
     return await OpenDialog(
-      title: 'Confirmar',
-      content: '¿Agregar el cliente?',
-      textButton1: 'Si',
-      textButton2: 'No',
-      context: context
+        title: 'Confirmar',
+        content: '¿Agregar el cliente?',
+        textButton1: 'Si',
+        textButton2: 'No',
+        context: context
     ).view();
   }
 
@@ -53,168 +198,4 @@ class _AddCustomerState extends State<AddCustomerScreen> {
     ).view();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: MediaQuery.of(context).size.width * 0.6,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black,
-            width: 1.0,
-          ),
-        ),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  controller: _dateController,
-                  decoration: InputDecoration(labelText: 'Fecha de ingreso'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa la fecha de ingreso';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _deletedController,
-                  decoration: InputDecoration(labelText: 'Deleted'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el estado de eliminación';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _dependentIdController,
-                  decoration: InputDecoration(labelText: 'ID Dependiente'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el ID del dependiente';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _documentController,
-                  decoration: InputDecoration(labelText: 'Documento'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el documento';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _lastnameController,
-                  decoration: InputDecoration(labelText: 'Apellido'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el apellido';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _nameController,
-                  decoration: InputDecoration(labelText: 'Nombre'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el nombre';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _notesController,
-                  decoration: InputDecoration(labelText: 'Notas'),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa las notas';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _partnerIdController,
-                  decoration: InputDecoration(labelText: 'ID del Partner'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el ID del partner';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _paymentNumberController,
-                  decoration: InputDecoration(labelText: 'Número de Pago'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el número de pago';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _telephoneController,
-                  decoration: InputDecoration(labelText: 'Teléfono'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el teléfono';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _userIdController,
-                  decoration: InputDecoration(labelText: 'ID de Usuario'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Por favor, ingresa el ID de usuario';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (await _confirm() == 1) {
-                          _submitForm();
-                        }
-                      },
-                      child: const Text('Aceptar'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        if (await _cancel() == 1) {
-                          _cancelForm();
-                        }
-                      },
-                      child: const Text('Cancelar'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
